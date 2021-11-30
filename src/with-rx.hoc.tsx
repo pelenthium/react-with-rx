124a774b46eb35235fa42e2ab60f668c98140ac5
pelenthium <pelenthium@gmail.com>
2021-11-30 22:55:51 +0800

    useMemo to subject to avoid lost updates of props + update eslint

--- src/with-rx.hoc.tsx
+++ src/with-rx.hoc.tsx
@@ -1,12 +1,15 @@
-import React, { ComponentType, useLayoutEffect, useState } from 'react'
+import React, { ComponentType, useLayoutEffect, useMemo, useState } from 'react'
 import { merge, Observable, Subject } from 'rxjs'
 import { map, scan } from 'rxjs/operators'
 
-export type Observify<P extends object> = {
+export type Observify<P extends Record<string, unknown>> = {
   readonly [K in keyof P]: Observable<P[K]>
 }
 
-export type WithRXSelectorResult<P extends object, D extends Partial<P>> = {
+export type WithRXSelectorResult<
+  P extends Record<string, unknown>,
+  D extends Partial<P>
+> = {
   props?: Partial<Observify<P>>
   defaultProps?: D
   effects$?: Observable<unknown>
@@ -16,50 +19,59 @@ export type WithRXSelectorResult<P extends object, D extends Partial<P>> = {
  * curried for better type inference
  * @see https://github.com/Microsoft/TypeScript/issues/15005#issuecomment-430588884
  */
-export const withRX = <P extends object>(Target: ComponentType<P>) => <
-  D extends Partial<P>
->(
-  selector: (props: Observable<Readonly<P>>) => WithRXSelectorResult<P, D>
-) => {
-  const Hoc = (origin: P) => {
-    const subject = new Subject<P>()
-    const [state, setState] = useState<Partial<P> | undefined>(undefined)
+export const withRX =
+  // eslint-disable-next-line @typescript-eslint/ban-types
 
-    useLayoutEffect(() => {
-      const { defaultProps, props, effects$ } = selector(subject)
-      setState(defaultProps || {})
-      if (props) {
-        const inputs: Observable<Partial<P>>[] = Object.keys(props).map((key) =>
-          // @ts-ignore
-          props[key].pipe(map((value) => ({ [key]: value })))
-        )
-        const result = merge(...inputs)
-        const subscription = result
-          .pipe(scan((a, b) => Object.assign({}, a, b)))
-          .subscribe((values) =>
-            setState((prevState) => Object.assign({}, prevState, values))
-          )
-        const effectsSubscription = effects$ ? effects$.subscribe() : undefined
-        return () => {
-          subscription.unsubscribe()
-          if (effectsSubscription) {
-            effectsSubscription.unsubscribe()
+
+    <P extends Record<string, unknown>>(Target: ComponentType<P>) =>
+    <D extends Partial<P>>(
+      selector: (props: Observable<Readonly<P>>) => WithRXSelectorResult<P, D>
+    ) => {
+      const Hoc = (origin: P) => {
+        const subject = useMemo(() => new Subject<P>(), [])
+        const [state, setState] = useState<Partial<P> | undefined>(undefined)
+
+        useLayoutEffect(() => {
+          const { defaultProps, props, effects$ } = selector(subject)
+          setState(defaultProps || {})
+          if (props) {
+            const inputs: Observable<Partial<Record<string, unknown>>>[] =
+              Object.keys(props)
+                .filter((key) => props[key] !== undefined)
+                .map((key) =>
+                  props[key]!.pipe(map((value) => ({ [key]: value })))
+                )
+            const result = merge(...inputs)
+            const subscription = result
+              .pipe(scan((a, b) => Object.assign({}, a, b)))
+              .subscribe((values) =>
+                setState((prevState) => Object.assign({}, prevState, values))
+              )
+            const effectsSubscription = effects$
+              ? effects$.subscribe()
+              : undefined
+            return () => {
+              subscription.unsubscribe()
+              if (effectsSubscription) {
+                effectsSubscription.unsubscribe()
+              }
+            }
           }
-        }
+          return undefined
+        }, [subject])
+
+        useLayoutEffect(() => {
+          subject.next(origin)
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, Object.values(origin))
+
+        return (
+          <React.Fragment>
+            {state &&
+              React.createElement(Target, Object.assign({}, origin, state))}
+          </React.Fragment>
+        )
       }
-      return undefined
-    }, [])
 
-    useLayoutEffect(() => {
-      subject.next(origin)
-    }, Object.values(origin))
-
-    return (
-      <React.Fragment>
-        {state && React.createElement(Target, Object.assign({}, origin, state))}
-      </React.Fragment>
-    )
-  }
-
-  return Hoc as ComponentType<Partial<P>>
-}
+      return Hoc as ComponentType<Partial<P>>
+    }
